a111e73826b80e4f5eb6176253c0eb1caa7db372
Alex State <astate@treancorp.com>
2019-01-26 16:15:41 -0800

    First commit

---
+++ Assets/Scripts/Parts.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parts : MonoBehaviour
+{
+    [SerializeField]
+    private float forceMultiplier = 1.75f;
+
+    [SerializeField]
+    private float xDir = 0.0f;
+
+    [SerializeField]
+    private float yDir = 0.0f;
+
+    [SerializeField]
+    private float xRot = 0.0f;
+
+    [SerializeField]
+    private float yRot = 0.0f;
+
+    private bool Bounce = false;
+    private float xForce;
+    private float yForce;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        xForce = Random.Range(-50f, 50f);
+        yForce = Random.Range(-50f, 50f);
+        xDir = Random.Range(-.5f, .5f);
+        yDir = Random.Range(-.5f, .5f);
+        xRot = Random.Range(-.1f, 5f);
+        yRot = Random.Range(-.1f, 5f);
+
+        rb.AddForce(xForce, yForce, 0, ForceMode.Impulse);
+    }
+
+    void FixedUpdate()
+    {
+        //rb.AddForce(xForce, yForce, 0, ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+        //GetComponent<Rigidbody>().transform.Translate(xDir, yDir, 0f);
+        //GetComponent<Rigidbody>().transform.Rotate(xRot, 0, 0f);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!Bounce)
+        {
+            xDir -= xDir;
+            yDir -= yDir;
+            xForce -= xForce*forceMultiplier;
+            yForce -= yForce*forceMultiplier;
+            rb.AddForce(xForce, yForce, 0, ForceMode.Impulse);
+            //Bounce = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private bool down = false;
+    [SerializeField] private Vector2 mouseOnDown;
+    [SerializeField] private Vector2 mouse;
+    [SerializeField] private float movementSpeed = .05f;
+    [SerializeField] private float limitXMovementMin = -45f;
+    [SerializeField] private float limitXMovementMax = 45f;
+    [SerializeField] private float limitYMovementMin = -20f;
+    [SerializeField] private float limitYMovementMax = 20f;
+    [SerializeField] private Vector3 position;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            down = true;
+            mouseOnDown.x = Input.mousePosition.x;
+            mouseOnDown.y = Input.mousePosition.y;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            down = false;
+        }
+
+        if (down || 1==1)
+        {
+            mouse.x = Input.mousePosition.x-725f;
+            mouse.y = Input.mousePosition.y-350f;
+
+            Debug.Log("x=" + mouse.x.ToString());
+            Debug.Log("y=" + mouse.y.ToString());
+
+            mouse.x *= movementSpeed;
+            mouse.y *= movementSpeed;
+
+            Debug.Log("damp x=" + mouse.x.ToString());
+            Debug.Log("damp y=" + mouse.y.ToString());
+
+            mouse.x = Mathf.Clamp(mouse.x, limitXMovementMin, limitXMovementMax);
+            mouse.y = Mathf.Clamp(mouse.y, limitYMovementMin, limitYMovementMax);
+
+            Debug.Log("clamp x=" + mouse.x.ToString());
+            Debug.Log("clamp y=" + mouse.y.ToString());
+
+            position.x = mouse.x; 
+            position.y = mouse.y; 
+            position.z = 0f; 
+            transform.position = position;
+        }
+    }
+}
