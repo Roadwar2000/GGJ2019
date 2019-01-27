c3421f90255689d9de1bc8f74d3e908a109130df
Ken D <kdel9105@gmail.com>
2019-01-26 19:24:48 -0800

    Added Home scene with scripts and materials

---
+++ Assets/Scripts/GoMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoMain : MonoBehaviour
+{
+    public void GoToMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
